ca912cbfe96d777efd328c2374b975ba389a4e24
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-13 01:10:53 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,5 +6,5 @@ class BirdsController < ApplicationController
   
     def show
       bird = Bird.find_by(id: params[:id])
-      render json: {id: bird.id, name: bird.name, species: bird.species } 
+      render json: bird.slice(:id, :name, :species)
     end
