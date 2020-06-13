3300869afadce0be5402d9715371378ba3cdf2fc
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-13 01:10:01 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -4,8 +4,7 @@ class BirdsController < ApplicationController
     render json: @birds
   end
   
-  def show
-    birds = Bird.all
-    render json: birds, except: [:created_at, :updated_at]
-  end
-end
+    def show
+      bird = Bird.find_by(id: params[:id])
+      render json: {id: bird.id, name: bird.name, species: bird.species } 
+    end
